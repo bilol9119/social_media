7d362d4db4b636516f46e84067fb83667d1af74f
Bilol <norovbilol71@gmail.com>
2024-03-10 08:06:45 +0300

    well already finished

--- authorization/urls.py
+++ authorization/urls.py
@@ -4,7 +4,7 @@ from .views import signup_view, login_view, logout_view
 
 
 urlpatterns = [
-    path('auth/login/', login_view, name='login'),
-    path('auth/register/', signup_view, name='register'),
-    path('auth/logout/', logout_view, name='logout'),
+    path('login/', login_view, name='login'),
+    path('register/', signup_view, name='register'),
+    path('logout/', logout_view, name='logout'),
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,23 @@
+import profile
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from .views import (home_view, account_settings_view,
                     profile_view, following_view,
-                    comment_save)
+                    comment_save, profile_view2,
+                    search_view, like_view)
 
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('account/', account_settings_view, name='account'),
-    path('profile/', profile_view, name='profile'),
+    path('profile/', profile_view2, name='profile'),
     path('follow/<int:id>/', following_view, name='follow'),
     path('commentsave', comment_save, name='commentsave'),
+    path('profile/<int:id>/', profile_view),
+    path('search', search_view, name='search'),
+    path('like/<int:id>/', like_view, name='like')
 
 ]
 
