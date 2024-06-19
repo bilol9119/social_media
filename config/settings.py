1c360bd3b0dcd239946eb1b7e74e7fdc5bacfa24
bilol <bilolnorov7@gmail.com>
2024-06-19 20:54:23 +0500

    authentication

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q9cpzim1is&m5p@cr6+(7$2*mzrfg9tl+-b-l1xn84tbu7yu1&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
